3d22aacdeebac377ac0bb1189ac6697f77e0f7ac
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-10 23:39:44 +0000

    create and use string

--- src/examples/01_data_types/05_string/main.cpp
+++ src/examples/01_data_types/05_string/main.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
+#include <string>
+
+using std::string; using std::cout; using std::endl;
 
 //create string variable, initialize and display
 int main()
 {
+    string greeting = "Hello, World!";
+    cout << greeting << endl;
 
     return 0;
 }
