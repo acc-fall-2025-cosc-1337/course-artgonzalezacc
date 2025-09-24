15cc2c51aceec16bbd876d4539a1a36431a1a47c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-24 23:01:14 +0000

    do while user controlled loop

--- src/examples/04_repetition/03_do_while/do_while.cpp
+++ src/examples/04_repetition/03_do_while/do_while.cpp
@@ -1,7 +1,20 @@
 #include "do_while.h"
 
+using std::cout;
+using std::cin;
+using std::endl;
+
 //Write code for void function prompt_user to loop until
-//user opts not to continue.  
+//user opts not to continue. 
+void prompt_user() {
+    char choice;
+    do 
+    {
+        cout << "Hello, User!" << std::endl;
+        cout << "Would you like to continue? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+} 
 
 
 
