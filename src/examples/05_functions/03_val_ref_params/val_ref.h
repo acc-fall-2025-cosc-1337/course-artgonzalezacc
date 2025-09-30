ef73eb34b37f0956b2d88c493cfbb0ba0d4f3631
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-30 04:42:11 +0000

    function value/reference parameter

---
+++ src/examples/05_functions/03_val_ref_params/val_ref.h
@@ -0,0 +1,2 @@
+void func_value_param(int num);
+void func_ref_param(int &num);
--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,19 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "val_ref.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test Value Parameter", "verification") {
+	int num = 10;// original value(variable)
+	func_value_param(num);
+	REQUIRE(num == 10);
+}
+
+TEST_CASE("Test Reference Parameter", "verification") {
+	int num = 10;// original value(variable)
+	func_ref_param(num);
+	REQUIRE(num == 30);
 }
