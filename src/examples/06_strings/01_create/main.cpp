4ab43cdb175b5c2d17841c53dc918c24deb3e2b0
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-10-06 23:37:31 +0000

    string size and capacity

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,4 +1,66 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::string;
+
 int main()
 {
+    string name = "C++";
+    cout<<name<<"\n";
+
+    cout<<"size of string: "<<name.size()<<"\n";
+    cout<<"capacity of string: "<<name.capacity()<<"\n";
+
+    name.append("i");
+
+    cout<<"size of string: "<<name.size()<<"\n";
+    cout<<"capacity of string: "<<name.capacity()<<"\n";
+
+    name.append("s");
+    name.append(" ");
+    name.append("c");
+    name.append("o");
+    name.append("o");
+    name.append("l");
+
+    cout<<"size of string: "<<name.size()<<"\n";
+    cout<<"capacity of string: "<<name.capacity()<<"\n";
+
+    name.append(" ");
+    name.append("c");
+    name.append("o");
+    name.append("o");
+    name.append("l");
+
+    cout<<name<<"\n";
+    cout<<"size of string: "<<name.size()<<"\n";
+    cout<<"capacity of string: "<<name.capacity()<<"\n";
+
+    name.append("!");
+
+    cout<<name<<"\n";
+    cout<<"size of string: "<<name.size()<<"\n";
+    cout<<"capacity of string: "<<name.capacity()<<"\n";
+
+    name.append(" ");
+    name.append("c");
+    name.append("o");
+    name.append("o");
+    name.append("l");
+    name.append(" ");
+    name.append("c");
+    name.append("o");
+    name.append("o");
+    name.append("l");
+    name.append(" ");
+    name.append("c");
+    name.append("o");
+    name.append("o");
+    name.append("l");
+
+    cout<<name<<"\n";
+    cout<<"size of string: "<<name.size()<<"\n";
+    cout<<"capacity of string: "<<name.capacity()<<"\n";
     return 0;
 }
