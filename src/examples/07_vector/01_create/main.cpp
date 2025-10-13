505470885e499742bd4d441fa6a6aa6dcb13afe6
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-10-13 23:10:34 +0000

    vector push_back,  size and capacity

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,34 @@
+#include<iostream>
+#include<vector>
+
+using std::cout;
+using std::vector;
+
 int main()
 {
+    vector<int> nums;
+    cout<<"size of vector: "<<nums.size()<<"\n";
+    cout<<"capacity of vector: "<<nums.capacity()<<"\n";
+    
+    nums.push_back(1);
+    cout<<"size of vector: "<<nums.size()<<"\n";
+    cout<<"capacity of vector: "<<nums.capacity()<<"\n";
+
+    nums.push_back(10);
+    cout<<"size of vector: "<<nums.size()<<"\n";
+    cout<<"capacity of vector: "<<nums.capacity()<<"\n";
+
+    nums.push_back(5);
+    cout<<"size of vector: "<<nums.size()<<"\n";
+    cout<<"capacity of vector: "<<nums.capacity()<<"\n";
+
+    nums.push_back(15);
+    cout<<"size of vector: "<<nums.size()<<"\n";
+    cout<<"capacity of vector: "<<nums.capacity()<<"\n";
+
+    nums.push_back(20);
+    cout<<"size of vector: "<<nums.size()<<"\n";
+    cout<<"capacity of vector: "<<nums.capacity()<<"\n";
+
     return 0;
 }
