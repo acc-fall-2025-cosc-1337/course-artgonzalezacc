41d274001865a7fb6abbe4b502f2fdc709ba01c5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-10-15 22:57:52 +0000

    vector mmory addressing

--- src/examples/07_vector/02_functions/main.cpp
+++ src/examples/07_vector/02_functions/main.cpp
@@ -1,8 +1,21 @@
-#include "function_vec.h"
+#include<iostream>
+#include<vector>
+
+using std::cout;
+using std::vector;
 
 int main()
 {
-    loop_vector_w_for_range();
+    auto num = 5;
+    vector<int> nums{1, 5, 3};
+
+    cout<<"Address of num: "<<&num<<"\n";
+    cout<<"Address of nums: "<<&nums<<"\n";
+
+    cout<<nums[0]<<"\n";
+    cout<<"Address of number at index 0: "<<&nums[0]<<"\n";//gives us the address of value 1 in nums
+    cout<<"Address of number at index 1: "<<&nums[1]<<"\n";//gives us the address of value 5 in nums
+    cout<<"Address of number at index 0: "<<&nums[2]<<"\n";//gives us the address of value 3 in nums
 
     return 0;
 }
