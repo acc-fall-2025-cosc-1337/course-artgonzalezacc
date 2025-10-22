3a3450e8849ff6e60404789e1d70555823d16a36
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-10-22 23:01:13 +0000

    Creaet bank account class

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,1 +1,11 @@
 //bank_account.h
+class BankAccount
+{
+    //variables and functions go here
+public:
+    int get_balance() { return balance; }
+
+private:
+    int balance; //default value is 0
+
+};
--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,6 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("test get balance") 
+{
+	BankAccount account;//create a variable---> create an object
+	REQUIRE(0 == account.get_balance());
 }
