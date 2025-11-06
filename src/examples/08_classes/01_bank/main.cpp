a1f9589160bce0d0bfdf609aaf6cda2bcf5480a7
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-06 00:40:35 +0000

    intro to pointers

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,5 +1,7 @@
 #include "atm.h"
 #include "bank_account.h"
+#include "checking_account.h"
+#include "savings_account.h"
 #include "bank_account_db.h"
 #include <ctime>   // For time()
 
@@ -10,11 +12,11 @@ int main()
 	srand(static_cast<unsigned int>(time(0)));
 	BankAccountDB db;
 	vector<BankAccount> accounts;
-	accounts.push_back(BankAccount(db.get_balance()));
+	accounts.push_back(SavingsAccount(db.get_balance()));
 	
-	accounts.push_back(BankAccount(db.get_balance()));
+	accounts.push_back(SavingsAccount(db.get_balance()));
 
-	accounts.push_back(BankAccount(db.get_balance()));
+	accounts.push_back(CheckingAccount(db.get_balance()));
 
 	ATM atm(accounts);
 
