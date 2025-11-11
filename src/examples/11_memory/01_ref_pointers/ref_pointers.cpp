7bc8558dfe1f3fb5df7ffaa4246cf4cffd64fe4d
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-11 00:29:18 +0000

    value, reference, and pointer params

--- src/examples/11_memory/01_ref_pointers/ref_pointers.cpp
+++ src/examples/11_memory/01_ref_pointers/ref_pointers.cpp
@@ -1,4 +1,10 @@
 #include "ref_pointers.h"
 
 //
+void val_ref_ptr_params(int num0, int& num1, int* num2)
+{
+    num0 = 10; //working with a copy
+    num1 = 20; //working with the original memory location of the param
+    *num2 = 30;  //working with the original memory location of the param
+}
 
