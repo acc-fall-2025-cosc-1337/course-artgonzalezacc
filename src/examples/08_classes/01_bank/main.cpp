e7279a94c2d557e7d96cc6250c0744dc2de9a4ad
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-12 23:59:24 +0000

    point to bank accounts

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -11,7 +11,17 @@ int main()
 {
 	srand(static_cast<unsigned int>(time(0)));
 	BankAccountDB db;
-	vector<BankAccount*> accounts;
+	BankAccount* ptr_account;	
+	
+	CheckingAccount checking(db.get_balance());//object (it's a variable)
+	ptr_account = &checking; //pointers work with addresses
+	std::cout<<ptr_account->get_balance()<<"\n";
+
+	SavingsAccount savings(db.get_balance());
+	ptr_account = &savings;
+	std::cout<<ptr_account->get_balance()<<"\n";
+
+	/*vector<BankAccount*> accounts;
 	SavingsAccount savings0 = SavingsAccount(db.get_balance());
 	accounts.push_back(&savings0);
 	
@@ -23,7 +33,7 @@ int main()
 
 	ATM atm(accounts);
 
-	run_menu(atm);
+	run_menu(atm);*/
 
 	return 0;
 }
