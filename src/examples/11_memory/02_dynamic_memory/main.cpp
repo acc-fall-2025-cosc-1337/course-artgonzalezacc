0e742eaf138ea3e37dce428fc5f61255cd04782b
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-13 00:05:29 +0000

    create dynamic memory

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -1,5 +1,12 @@
+#include<iostream>
+
+using std::cout;
+
 int main() 
 {
-	
+	int* num = new int(10);//create dynamic memory(grap a memory address/block from the heap)
+	cout<<*num<<"\n";//for how long do i need the dynamic memory
+	delete num;
+
 	return 0;
 }
