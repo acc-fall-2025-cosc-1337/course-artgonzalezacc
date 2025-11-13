9729be8e21ff49fd4eba754091cc0f2c4b1cf7d0
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-13 00:24:29 +0000

    shared ptr example

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -2,12 +2,17 @@
 #include<memory>
 
 using std::cout;
-using std::unique_ptr; using std::make_unique;
+using std::shared_ptr; using std::make_shared;
 
 int main() 
 {
-	unique_ptr<int> num = make_unique<int>(150);//creates dynamic memory on the heap
+	shared_ptr<int> num = make_shared<int>(150);//creates dynamic memory on the heap
 	cout<<*num<<"\n";//use
+	cout<<"Shared count: "<<num.use_count()<<"\n";
+
+	shared_ptr<int> num1 = num;
+	cout<<*num<<"\n";//use
+	cout<<"Shared count: "<<num.use_count()<<"\n";
 
 	//unique ptr calls delete for us(right before main is removed from memory)
 	return 0;
