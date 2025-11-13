b387f6bc935df7daba42320fa4dfc250ab941ee9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-13 00:28:22 +0000

    weak ptr example

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -3,16 +3,22 @@
 
 using std::cout;
 using std::shared_ptr; using std::make_shared;
+using std::weak_ptr;
 
 int main() 
 {
 	shared_ptr<int> num = make_shared<int>(150);//creates dynamic memory on the heap
 	cout<<*num<<"\n";//use
-	cout<<"Shared count: "<<num.use_count()<<"\n";
+	cout<<"count: "<<num.use_count()<<"\n";
+	
+	weak_ptr<int> wp_num = num;
+	cout<<"count: "<<num.use_count()<<"\n";
 
-	shared_ptr<int> num1 = num;
-	cout<<*num<<"\n";//use
-	cout<<"Shared count: "<<num.use_count()<<"\n";
+	if(!wp_num.expired())
+	{
+		shared_ptr<int> sp1 = wp_num.lock();
+		cout<<*sp1<<"\n";
+	}
 
 	//unique ptr calls delete for us(right before main is removed from memory)
 	return 0;
