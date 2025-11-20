1006ecde4d2f6f4ccc79f4dea096c5e0a26f76fa
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-20 00:27:40 +0000

    academic create, use, and delete memory

--- src/examples/11_memory/02_dynamic_memory/dynamic_memory.cpp
+++ src/examples/11_memory/02_dynamic_memory/dynamic_memory.cpp
@@ -1,4 +1,17 @@
 #include "dynamic_memory.h"
-#include<iostream>
-//
+
+using std::cout;
+
+void use_dynamic_memory_array(const int SIZE)
+{
+    int* nums = new int[SIZE]{6, 1, 10};//create and initializes the dynamic array list on the heap
+
+    for(auto i=0; i < SIZE; i++)
+    {
+        cout<<nums[i]<<"\n";
+    }
+
+    delete[] nums;
+
+}
 
--- src/examples/11_memory/02_dynamic_memory/dynamic_memory.h
+++ src/examples/11_memory/02_dynamic_memory/dynamic_memory.h
@@ -1,2 +1,4 @@
 //Create allocate and release function
+#include<iostream>
 
+void use_dynamic_memory_array(const int SIZE);
