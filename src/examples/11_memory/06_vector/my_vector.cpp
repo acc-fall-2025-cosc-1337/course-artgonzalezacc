00f16b3a6821f14e8820a76ecf4872df9da25465
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-25 00:02:54 +0000

    my vector constructor/destructor

--- src/examples/11_memory/06_vector/my_vector.cpp
+++ src/examples/11_memory/06_vector/my_vector.cpp
@@ -1,4 +1,21 @@
 #include "my_vector.h"
 
-//
+using std::cout;
 
+//
+MyVector::MyVector() : size(0), capacity(0) //constructor
+{
+
+}
+
+MyVector::MyVector(int s)
+: size(s), capacity(s), elements{new int[size]}//create the dynamic memory of size s
+{
+    cout<<"Created elements memory at: "<<elements<<"\n";
+}
+
+MyVector::~MyVector()
+{
+    cout<<"Free memory at : "<<elements<<"\n";
+    delete[] elements;
+}
--- src/examples/11_memory/06_vector/my_vector.h
+++ src/examples/11_memory/06_vector/my_vector.h
@@ -1,1 +1,21 @@
 //
+#include<iostream>
+
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+
+class MyVector
+{
+
+public:
+    MyVector(); //blank/empty  list
+    MyVector(int s); //a list of size s
+    ~MyVector();//destructor
+private:
+    int size;
+    int capacity;
+    int* elements; //point to something eventually
+
+};
+
+#endif
