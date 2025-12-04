30fe01752e38d67267e0be69ef1d0ff49152bd60
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-12-04 00:05:33 +0000

    template function

--- src/examples/11_memory/05_class_copy/main.cpp
+++ src/examples/11_memory/05_class_copy/main.cpp
@@ -1,6 +1,20 @@
+#include<iostream>
+
+using std::cout;
+
+template<class T>
+T add(T a, T b)
+{
+	return a + b;
+}
+
 int main() 
 {
+	auto result = add(5, 5);
+	cout<<result<<"\n";
 
+	auto result1 = add(5.5, 5.7);
+	cout<<result1<<"\n";
 
 	return 0;
 }
